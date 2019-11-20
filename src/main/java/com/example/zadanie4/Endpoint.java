d0091fa1a17dd05fa05c26c455cd2a0b91714ec2
mcfb <maciekfab@gmail.com>
2019-11-20 20:49:24 +0100

    zadanie4

---
+++ src/main/java/com/example/zadanie4/Endpoint.java
@@ -0,0 +1,110 @@
+package com.example.zadanie4;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class Endpoint {
+
+    private BufferedWriter bw;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        Input input = new Input();
+        input.setName("");
+        model.addAttribute("input", input);
+        return "index";
+    }
+
+
+    @RequestMapping(value = "/result", method = RequestMethod.POST)
+    public String search(@ModelAttribute Input input, Model model) throws IOException {
+        model.addAttribute("searchName", input.name);
+
+        List<String> names = new ArrayList<>();
+        String url = buildUrl(input);
+        Document doc = Jsoup.connect(url).validateTLSCertificates(false).get();
+        Elements elements = doc.select("div.user-info");
+
+        for (Element element : elements) {
+            names.add(element.select("a").attr("title"));
+        }
+
+        model.addAttribute("names", names);
+        model.addAttribute("input", input);
+
+        return "result";
+    }
+
+
+    @RequestMapping(value = "/card/{id}", method = RequestMethod.GET)
+    public String card(@PathVariable String id, Model model) {
+        String[] data = id.split(" ");
+        writeFile(data);
+        return "card";
+    }
+
+    private void writeFile(String[] data) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("card");
+        builder.append(".vcf");
+
+        try {
+            File file = new File(builder.toString());
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            bw = new BufferedWriter(fw);
+            bw.write("BEGIN:VCARD" + System.lineSeparator());
+            bw.write("VERSION:2.1" + System.lineSeparator());
+            bw.write("FN:" + data[0] + " " + data[1]+ System.lineSeparator());
+            bw.write("N:" + data[0] + ";" + data[1] + System.lineSeparator());
+            bw.write("TEL;CELL:+48-602-602-602" + System.lineSeparator());
+            bw.write("END:VCARD");
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String buildUrl(@ModelAttribute("foo") Input input) {
+        String baseURL = "http://adm.edu.p.lodz.pl/user/users.php?search=";
+        String[] data = input.name.split(" ");
+        StringBuilder builder = new StringBuilder("");
+        for (int i = 0; i < data.length; i++) {
+            builder.append(data[i]);
+            builder.append("+");
+        }
+        return baseURL + builder.toString();
+    }
+
+
+    class Input {
+        String name;
+
+        public Input() {
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
---
+++ src/main/java/com/example/zadanie4/Zadanie4Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie4Application.class, args);
+	}
+
+}
